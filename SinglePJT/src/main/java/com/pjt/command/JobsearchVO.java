448a5a7d5b6c4f006e389561cbc186edc2677d77
user <user@DESKTOP-OQ8FJ1G>
2022-11-24 07:52:43 +0900

    SinglePjt - workaholic Json데이터 Mybatis 저장기능 구현

--- SinglePJT/src/main/java/com/pjt/command/JobsearchVO.java
+++ SinglePJT/src/main/java/com/pjt/command/JobsearchVO.java
@@ -1,45 +1,12 @@
 package com.pjt.command;
 
-import java.sql.CallableStatement;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Map;
-
-import org.apache.ibatis.type.BaseTypeHandler;
-import org.apache.ibatis.type.JdbcType;
-
 import lombok.Data;
 
 @Data
-public class JobsearchVO extends BaseTypeHandler<Object>{
+public class JobsearchVO{
 	private int jobsearch_num;
 	private String jobsearch_title;
-	private Map<String, Object> jobsearch_main;
+	private String jobsearch_main;
 	
-	@Override
-	public Object getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	@Override
-	public Object getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	@Override
-	public Object getNullableResult(ResultSet rs, String columnName) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	@Override
-	public void setNonNullParameter(PreparedStatement ps, int i, Object parameter, JdbcType jdbcType)
-			throws SQLException {
-		// TODO Auto-generated method stub
-		
-	}
 	
 }
--- SinglePJT/src/main/java/com/pjt/controller/RecruiterController.java
+++ SinglePJT/src/main/java/com/pjt/controller/RecruiterController.java
@@ -9,8 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.pjt.Service.jobsearchService;
 import com.pjt.command.JobsearchVO;
 
@@ -28,12 +32,17 @@ public class RecruiterController {
       return "/recruiter/Job_search";
    }
    
-   @PostMapping("/job_search")
+   @RequestMapping(value="/job_search", produces = "application/json; charset=utf8", method = RequestMethod.POST)
    @ResponseBody
-   public ResponseEntity<Integer> job_search(@RequestBody Map<String, Object> job_search ) {
+   public ResponseEntity<Integer> job_search(@RequestBody Map<String, Object> job_search ) throws JsonProcessingException {
       ResponseEntity<Integer> res=null;
       JobsearchVO vo = new JobsearchVO();
-      vo.setJobsearch_main(job_search);
+      ObjectMapper om = new ObjectMapper();
+      JSONPObject json = new JSONPObject("JSON.parse", job_search);
+      String jobsearch_main = om.writeValueAsString(json);
+      System.out.println(jobsearch_main);
+      
+      vo.setJobsearch_main(jobsearch_main);
       vo.setJobsearch_title("test타이틀");
       js.addJobSearch(vo);
       System.out.println("2\n "+job_search);
